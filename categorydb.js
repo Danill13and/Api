052087624d027e77f869c41654c06b0528588814
Danill13and <114337660+Danill13and@users.noreply.github.com>
2024-05-09 17:37:42 +0300

    Update categorydb.js

--- categorydb.js
+++ categorydb.js
@@ -1,7 +1,6 @@
 const { DataTypes, Sequelize } = require('sequelize')
 
-const sequelizer = new Sequelize('marketplaes', 'postgres', 'postgres', {
-    host: 'localhost',
+const sequelizer = new Sequelize('postgres://danill13and:WFbWTM5oQXT53pELtWkxqMeZKwnZlzWT@dpg-cot6hamv3ddc73fafh8g-a/marketplace_hr6r', {
     dialect: 'postgres'
 });
 
